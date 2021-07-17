5f195c50d5df01af6be97c7cb4de6e0d06f83f30
Elias Jimenez <eliasj1509@gmail.com>
2021-07-16 22:49:13 -0600

    Metido de rellenar actualizado

--- Controlador.cpp
+++ Controlador.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 void Controlador::rellenarLista(string entrada){
+    int contador=0;
     ListaVariables *l=new ListaVariables();
     bool estado=true;
     for (int i=0;i<entrada.size();i++){
@@ -15,7 +16,11 @@ void Controlador::rellenarLista(string entrada){
             Variable *n=new Variable(entrada[i],estado);
             l->agregarVariable(n);    
             estado=true;
-            if(entrada[i+1]=='+'&&i+1<entrada.size()||i==entrada.size()-1||i==entrada.size()-2){
+            bool condicionUno=i+1<entrada.size()-1&&entrada[i+1]=='+';
+            bool condicionDos=i+2<entrada.size()-2&&entrada[i+2]=='+';
+            bool condicionTres=i==entrada.size()-1;
+            bool condicionCuatro=entrada[i+1]=='\''&&i+2==entrada.size();
+            if(condicionUno||condicionDos||condicionTres||condicionCuatro){
                 Mintermino *m=new Mintermino(l);
                 lista->agregarMintermino(m);
                 l=nullptr;
