3be29dc051a64709d6ec5af1617ae68c31378cb2
danieljimenezm <82168294+danieljimenezm@users.noreply.github.com>
2021-07-23 10:08:32 -0600

    Se modifican líneas de bitácora.

--- Bitacora.h
+++ Bitacora.h
@@ -1,4 +1,13 @@
 #ifndef BITACORA_H
 #define BITACORA_H
+#include <string>
+
+class Bitacora{
+    std::string name;
+public:
+    Bitacora(){ name = "Bitacora.txt"; };
+    void write(std::string m);
+    std::string read();
+};
 
 #endif // BITACORA_H
